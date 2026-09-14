3515ad0d01c9ce3b693e3ded3c538a4a7efe2015
Emiliano Souza <emiliano.fs.junior@gmail.com>
2026-09-14 11:41:06 -0300

    feat: create product reviews module

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -384,6 +384,7 @@ return [
         'PayPal_BraintreeGiftWrapping' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'PayPal_BraintreeReward' => 1,
-        'Webjump_Emiliano' => 1
+        'Webjump_Emiliano' => 1,
+        'Webjump_ProductReviews' => 1
     ]
 ];
